024ba27a1cdeeaee231c048ae37a6a7797debf88
Praisebuka <praise.njoga@gmail.com>
2025-06-17 21:35:18 +0100

    mail config trouble shooting

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Mail\SendQuickMSG;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
