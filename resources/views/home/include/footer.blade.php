2ad0ff7df5292012365d9d347f46b162592f0a44
Praisebuka <praise.njoga@gmail.com>
2025-06-17 16:26:28 +0100

    footer links corrected :)

--- resources/views/home/include/footer.blade.php
+++ resources/views/home/include/footer.blade.php
@@ -4,7 +4,6 @@
         <div class="footer__content grid">
             <div class="footer__data">
                 <h3 class="footer__title"> Matrix Recruitment </h3>
-                {{-- <p class="footer__description"> Travel you choose the <br> destination, we offer you the <br> experience. </p> --}}
                 <p class="footer__description"> We specialize in supplying <br> high-quality, ready-to-work staffs <br> to businesses across London</p>
                 <div>
                     <a href="https://www.facebook.com/" target="_blank" class="footer__social">
@@ -26,13 +25,13 @@
                 <h3 class="footer__subtitle"> About </h3>
                 <ul>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> About Us </a>
+                        <a href="{{ route('about us') }}" class="footer__link"> About Us </a>
                     </li>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> Our Services </a>
+                        <a href="/#services" class="footer__link"> Our Services </a>
                     </li>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> News & Blog </a>
+                        <a href="#" class="footer__link" title="Coming Soon :)"> News & Blog </a>
                     </li>
                 </ul>
             </div>
@@ -41,13 +40,13 @@
                 <h3 class="footer__subtitle"> Company </h3>
                 <ul>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> Team </a>
+                        <a href="#" class="footer__link" title="Coming Soon :)"> Team </a>
                     </li>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> Plan & Pricing </a>
+                        <a href="{{ route('contact us') }}" class="footer__link"> Plan & Pricing </a>
                     </li>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> Become a Partner </a>
+                        <a href="{{ route('contact us') }}" class="footer__link"> Become a Partner </a>
                     </li>
                 </ul>
             </div>
@@ -56,13 +55,13 @@
                 <h3 class="footer__subtitle"> Support </h3>
                 <ul>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> FAQs </a>
+                        <a href="#" class="footer__link" title="Coming Soon :)"> FAQs </a>
                     </li>
                     <li class="footer__item">
                         <a href="mailto:hr@matrixrecruitment.org" class="footer__link"> Support Center </a>
                     </li>
                     <li class="footer__item">
-                        <a href="" class="footer__link"> Contact Us </a>
+                        <a href="{{ route('contact us') }}" class="footer__link"> Contact Us </a>
                     </li>
                 </ul>
             </div>
@@ -71,8 +70,8 @@
         <div class="footer__rights">
             <p class="footer__copy">&#169; 2025 Matrix Recruitment. All rigths reserved. </p>
             <div class="footer__terms">
-                <a href="#" class="footer__terms-link"> Terms & Agreements </a>
-                <a href="#" class="footer__terms-link"> Privacy Policy </a>
+                <a href="#" class="footer__terms-link" title="Coming Soon"> Terms & Agreements </a>
+                <a href="#" class="footer__terms-link" title="Coming Soon"> Privacy Policy </a>
             </div>
         </div>
     </div>
