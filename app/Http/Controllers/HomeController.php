8387bdaa7e5ee36085bfb060044bb94373ab2266
Praisebuka <praise.njoga@gmail.com>
2025-06-17 22:23:24 +0100

    the correct mail addr to send to

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,7 +62,7 @@ class HomeController extends Controller
             ]);
 
             $message = SendQuickMessage::create($validatedData);
-            Mail::to('praise.njoga@gmail.com')->cc($message->email)->send(new SendQuickMSG($message));
+            Mail::to('info@matrixrecruitment.org')->cc($message->email)->send(new SendQuickMSG($message));
 
             return redirect()->back()->with('success', 'Thank you for reaching out to us, your message has been sent and we will be in touch!');
         
