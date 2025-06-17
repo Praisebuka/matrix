fe64ccbac62d3352f4ecd122259b2eaba11404c3
Praisebuka <praise.njoga@gmail.com>
2025-06-17 21:35:46 +0100

    resp msg

--- resources/views/home/include/footerjs.blade.php
+++ resources/views/home/include/footerjs.blade.php
@@ -7,6 +7,41 @@
 
         <!--=============== MAIN JS ===============-->
         <script src="{{ asset('assets/js/main.js') }}"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+        @if(session('success'))
+            <script>
+                swal.fire({
+                    icon: 'success',
+                    title: 'Success',
+                    text: '{{ session('success') }}',
+                    confirmButtonColor: '#3085d6',
+                });
+            </script>
+        @endif
+
+        @if(session('error'))
+            <script>
+                swal.fire({
+                    icon: 'error',
+                    title: 'Oops!',
+                    text: '{{ session('error') }}',
+                    confirmButtonColor: '#d33',
+                });
+            </script>
+        @endif
+
+        @if(session('resp'))
+            <script>
+                swal.fire({
+                    icon: '{{ session('resp.icon') }}',
+                    title: '{{ session('resp.title') }}',
+                    text: '{{ session('resp.text') }}',
+                    confirmButtonColor: '#d33',
+                });
+            </script>
+        @endif
+
 
 
     </body>
